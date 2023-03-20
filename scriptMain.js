7978a8e7e7805f2caceb201c4a13a51351588990
Eduardo Almeida <75376684+eduardo-almeida-projects@users.noreply.github.com>
2023-03-20 20:11:04 -0300

    Add files via upload

---
+++ scriptMain.js
@@ -0,0 +1,5 @@
+function scrollToElement() {
+    console.log('FUNCIONANDO')
+    var elemento = document.getElementById("#"); //INSIRA ALGO AQUI APÓS CRIAR O CORPO DO SITE
+    elemento.scrollIntoView({ behavior: "smooth" });
+}
