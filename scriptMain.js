464dad0ac43232d0e8cd15fc447068b3ef6e6629
Eduardo Almeida <75376684+eduardo-almeida-projects@users.noreply.github.com>
2023-03-24 22:28:54 -0300

    Update scriptMain.js

--- scriptMain.js
+++ scriptMain.js
@@ -1,5 +1,13 @@
+var camisa1 = document.getElementById('camisaUmBox');
+
 function scrollToElement() {
     console.log('FUNCIONANDO')
     var elemento = document.getElementById("#"); //INSIRA ALGO AQUI APÓS CRIAR O CORPO DO SITE
     elemento.scrollIntoView({ behavior: "smooth" });
+}
+function shineCamisaUmEnter() {
+    camisa1.style = 'border: 1px solid rgb(166, 255, 0)'
+}
+function shineCamisaUmLeave() {
+    camisa1.style = 'border: 0px'
 }
